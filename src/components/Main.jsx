ad69240a5b114b9ae960e3a20a6accb21ede264d
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-08-16 23:36:51 +0530

    Changes in Style
    New changes are made in the style and design of webpage.
    
    Date: 16 August 2023

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,48 +2,53 @@ import React from "react"
 import Footer from "./Footer"
 
 export default function Main() {
+
+    function Clicked(){
+        console.log("Clicked!");
+    }
+
     return (
-        <div class="container-main">
-            <div class="container">
+        <div className="container-main">
+            <div className="container">
 
-                <div class="con1-div1">
-                    <div class="con1-div1-1">
-                        <div class="con1-div1-1-1">Day</div>
-                        <div class="con1-div1-1-2">
-                            <input class="input--1" type="text" placeholder="DD" />
+                <div className="con1-div1">
+                    <div className="con1-div1-1">
+                        <div className="con1-div1-1-1">Day</div>
+                        <div className="con1-div1-1-2">
+                            <input className="input--1" type="text" name="day" placeholder="DD" />
                         </div>
                     </div>
 
-                    <div class="con1-div1-2">
-                        <div class="con1-div1-2-1">Month</div>
-                        <div class="con1-div1-2-2">
-                            <input class="input--2" type="text" placeholder="MM" />
+                    <div className="con1-div1-2">
+                        <div className="con1-div1-2-1">Month</div>
+                        <div className="con1-div1-2-2">
+                            <input className="input--2" type="text" name="month" placeholder="MM" />
                         </div>
                     </div>
 
-                    <div class="con1-div1-3">
-                        <div class="con1-div1-3-1">Year</div>
-                        <div class="con1-div1-3-2">
-                            <input class="input--3" type="text" placeholder="YYYY" />
+                    <div className="con1-div1-3">
+                        <div className="con1-div1-3-1">Year</div>
+                        <div className="con1-div1-3-2">
+                            <input className="input--3" type="text" name="year" placeholder="YYYY" />
                         </div>
                     </div>
 
-                    <div class="con1-div1-4">
+                    <div type="submit" className="con1-div1-4" onClick={Clicked}>
                         
                     </div>
                 </div>
 
-                <div class="con1-div2">
-                    <div class="con1-div2-1">
-                        <div class="con1-div2-1-1"><span class="span-1">--</span>years</div>
+                <div className="con1-div2">
+                    <div className="con1-div2-1">
+                        <div className="con1-div2-1-1"><span className="span-1">--</span>years</div>
                     </div>
 
-                    <div class="con1-div2-2">
-                        <div class="con1-div2-2-1"><span class="span-2">--</span>months</div>
+                    <div className="con1-div2-2">
+                        <div className="con1-div2-2-1"><span className="span-2">--</span>months</div>
                     </div>
 
-                    <div class="con1-div2-3">
-                        <div class="con1-div2-3-1"><span class="span-3">--</span>days</div>
+                    <div className="con1-div2-3">
+                        <div className="con1-div2-3-1"><span className="span-3">--</span>days</div>
                     </div>
                 </div>
 
